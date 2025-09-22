810ce1bd6162d41e4459e45543b44be2bbf26709
coder-muller <guilhermemullerxx@gmail.com>
2025-09-22 10:53:26 -0300

    feat: user's dropdown menu completed;

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,13 +1,22 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { LogOut, Plus, Sun, User } from "lucide-react";
+import { Crown, LogOut, Moon, Plus, Sun, User } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function HomeLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const { setTheme, theme } = useTheme();
+
+    const handleToggleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    }
+
     return (
         <div className="w-full h-screen flex flex-col">
             {/* header */}
@@ -26,25 +35,29 @@ export default function HomeLayout({
                             </Button>
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="end">
-                            <DropdownMenuItem>
+                            <DropdownMenuItem className="cursor-pointer">
                                 <div className="flex items-center gap-2">
                                     <Avatar className="size-8 rounded-md">
                                         <AvatarImage src="https://github.com/coder-muller.png" />
                                         <AvatarFallback>JD</AvatarFallback>
                                     </Avatar>
                                     <div className="flex flex-col">
-                                        <p className="text-sm font-medium">John Doe</p>
-                                        <p className="text-sm text-muted-foreground">john.doe@example.com</p>
+                                        <div className="flex items-center gap-2">
+                                            <p className="text-sm font-medium">Guilherme Müller</p>
+                                            <Crown className="size-3 text-yellow-500" />
+                                        </div>
+                                        <p className="text-sm text-muted-foreground">guilherme.muller@example.com</p>
                                     </div>
                                 </div>
                             </DropdownMenuItem>
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem>
-                                <Sun />
-                                Light Mode
+                            <DropdownMenuItem onClick={handleToggleTheme} className="group cursor-pointer">
+                                <Sun className="dark:block hidden group-hover:rotate-90 transition-transform duration-300 ease-in-out" />
+                                <Moon className="block dark:hidden group-hover:rotate-360 transition-transform duration-300 ease-in-out" />
+                                Toggle Theme
                             </DropdownMenuItem>
                             <DropdownMenuSeparator />
-                            <DropdownMenuItem className="hover:text-destructive focus:text-destructive group transition-colors duration-300">
+                            <DropdownMenuItem className="hover:text-destructive focus:text-destructive group transition-colors duration-300 cursor-pointer">
                                 <LogOut className="group-hover:text-destructive group-focus:text-destructive transition-colors duration-300" />
                                 Logout
                             </DropdownMenuItem>
